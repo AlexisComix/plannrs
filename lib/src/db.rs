b304767aec87b9daa499641191b5f21e7f5f1702
Alexis Williams <Thecomixguy@hotmail.com>
2024-06-24 12:19:45 +0100

    update docstrings

--- lib/src/db.rs
+++ lib/src/db.rs
@@ -39,13 +39,46 @@ pub struct Tag {
 /// the sake of abstraction here to use the whole struct as we will be fetching
 /// it whenever we want to fetch the tag anyway.
 pub struct Plan {
+    /// The Id for each entry in the plan table. It would be possible to 
+    /// have the primary key be the start datetime as I do not plan to allow 
+    /// users to have two study sessions at once (I do not think that it would
+    /// be feasible to rewrite all of the other systems just to accomodate it).
+    /// However, I think that I may as well just use IDs for it as it will always
+    /// be simpler.
     pub(crate) id: u8,
+    /// This is the description of the plan or study session. A user could
+    /// write a short or a longer description (we would have to have a button to 
+    /// toggle an expanded popup for the text, and possibly limit the number 
+    /// of characters based off of that larger view. Most people will not see
+    /// this part anyway, but it is worth thinking about.)
     pub(crate) description: String,
+    /// This is the start time for any given study session. It is calculated
+    /// based off of the local time on the relevant system. This should not
+    /// cause any issues in future, perhaps unless someone changes timezones
+    /// before the notification appears for a given plan.
     pub(crate) start: DateTime<Local>,
+    /// Similar to the start time, this should only have issues if the user
+    /// changes timezone. This will probably be an unlikely circumstance.
+    /// However, I think that when they are in the new timezone, the displayed
+    /// time will update to match so the user can change the time in the DB 
+    /// manaully. There could also be a warning in the daemon that triggers
+    /// a notification to do so if the timezone has changed. 
     pub(crate) until: DateTime<Local>,
+    /// This is the amount of time before the start that the notification should
+    /// appear. We will only use the seconds for this most likely, in increments
+    /// of 60 for minutes.
     pub(crate) advance: Option<TimeDelta>,
+    /// This is a flag for if the task has been completed or done. This can
+    /// be a user changed checkmark on each task.
     pub(crate) done: bool,
+    /// This is the option for the plan to be associated with a tag. If it 
+    /// is not associated with a tag, it will have a default grey/black/white
+    /// colour scheme in the timeline. 
     pub(crate) tag: Option<Tag>,
+    /// This is a flag for if the notification will sound on the desktop
+    /// or not. 
     pub(crate) notify: bool,
+    /// This is a flag for porsmo integration. Planned, but not likely anytime
+    /// soon.
     pub(crate) porsmo: bool,
 }
